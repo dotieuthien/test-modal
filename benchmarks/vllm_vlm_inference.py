4282b3661b3b8a3a2d882db0463c834f232b2af3
dotieuthien <dotieuthien9997@gmail.com>
2025-06-02 10:34:52 +0700

    Test to custom router

--- benchmarks/vllm_vlm_inference.py
+++ benchmarks/vllm_vlm_inference.py
@@ -2,7 +2,8 @@ import modal
 
 
 vllm_image = (
-    modal.Image.from_registry("nvidia/cuda:12.4.0-devel-ubuntu22.04", add_python="3.12")
+    modal.Image.from_registry(
+        "nvidia/cuda:12.4.0-devel-ubuntu22.04", add_python="3.12")
     .pip_install(
         "GPUtil",
         "vllm==v0.8.3",
@@ -19,7 +20,8 @@ volume = modal.Volume.from_name("llama_models", create_if_missing=True)
 app = modal.App("example-vllm-openai-compatible")
 
 N_GPU = 1  # tip: for best results, first upgrade to more powerful GPUs, and only then increase GPU count
-TOKEN = "super-secret-token"  # auth token. for production use, replace with a modal.Secret
+# auth token. for production use, replace with a modal.Secret
+TOKEN = "super-secret-token"
 
 MINUTES = 60  # seconds
 HOURS = 60 * MINUTES
@@ -35,7 +37,9 @@ HOURS = 60 * MINUTES
 )
 @modal.asgi_app()
 def serve():
+    import os
     import fastapi
+    from fastapi import Request
     import vllm.entrypoints.openai.api_server as api_server
     from vllm.engine.arg_utils import AsyncEngineArgs
     from vllm.engine.async_llm_engine import AsyncLLMEngine
@@ -48,26 +52,25 @@ def serve():
     from vllm.entrypoints.openai.serving_models import (BaseModelPath,
                                                         OpenAIServingModels)
     from vllm.usage.usage_lib import UsageContext
-    
-    
+
     # # set export VLLM_USE_V1=1 in python script
     # import os
     # os.environ["VLLM_USE_V1"] = "1"
-    
-    
+
     def print_system_info():
         import psutil
         import GPUtil
 
         # Memory info
         mem = psutil.virtual_memory()
-        print(f"Memory: Total={mem.total / (1024**3):.2f}GB, Available={mem.available / (1024**3):.2f}GB")
-        
+        print(
+            f"Memory: Total={mem.total / (1024**3):.2f}GB, Available={mem.available / (1024**3):.2f}GB")
+
         # GPU info
         gpus = GPUtil.getGPUs()
         for i, gpu in enumerate(gpus):
-            print(f"GPU {i}: {gpu.name}, Memory Total={gpu.memoryTotal}MB, Memory Used={gpu.memoryUsed}MB")
-
+            print(
+                f"GPU {i}: {gpu.name}, Memory Total={gpu.memoryTotal}MB, Memory Used={gpu.memoryUsed}MB")
 
     volume.reload()  # ensure we have the latest version of the weights
 
@@ -80,17 +83,60 @@ def serve():
     )
 
     router = fastapi.APIRouter()
+    custom_router = fastapi.APIRouter()
+
+    from vllm.entrypoints.openai.protocol import (
+        ChatCompletionRequest,
+        ChatCompletionResponse,
+        ErrorResponse
+    )
+    from fastapi.responses import JSONResponse, Response, StreamingResponse
+
+    @custom_router.post("/generate")
+    async def generate(
+        request: ChatCompletionRequest,
+        raw_request: Request
+    ):
+
+        print("--------------------------------")
+        # print all headers
+        body = await raw_request.json()
+        print(body)
+        print("--------------------------------")
+
+        handler = api_server.chat(raw_request)
+        if handler is None:
+            return api_server.base(raw_request).create_error_response(
+                message="The model does not support Chat Completions API")
+
+        generator = await handler.create_chat_completion(request, raw_request)
+
+        if isinstance(generator, ErrorResponse):
+            return JSONResponse(content=generator.model_dump(),
+                                status_code=generator.code)
+
+        elif isinstance(generator, ChatCompletionResponse):
+            return JSONResponse(content=generator.model_dump())
+
+        return StreamingResponse(content=generator, media_type="text/event-stream")
 
     # wrap vllm's router in auth router
     router.include_router(api_server.router)
+    router.include_router(custom_router)
+
     # add authed vllm to our fastAPI app
     web_app.include_router(router)
+    
+    # set VLLM_API_KEY in environment
+    os.environ["VLLM_API_KEY"] = TOKEN
 
     engine_args = AsyncEngineArgs(
         model=MODELS_DIR + "/" + MODEL_NAME,
         gpu_memory_utilization=0.90,
         max_model_len=8096,
-        enforce_eager=False,  # capture the graph for faster inference, but slower cold starts (30s > 20s)
+        # capture the graph for faster inference, but slower cold starts (30s > 20s)
+        enforce_eager=False,
+        
     )
 
     engine = AsyncLLMEngine.from_engine_args(
@@ -104,7 +150,7 @@ def serve():
     base_model_paths = [
         BaseModelPath(name=MODEL_NAME.split("/")[1], model_path=MODEL_NAME)
     ]
-    
+
     openai_serving_models = OpenAIServingModels(
         engine,
         model_config=model_config,
@@ -112,10 +158,20 @@ def serve():
         lora_modules=[],
         prompt_adapters=[],
     )
-    
+
     api_server.models = lambda s: openai_serving_models
 
     api_server.chat = lambda s: OpenAIServingChat(
+        engine,
+        model_config,
+        openai_serving_models,
+        chat_template=None,
+        chat_template_content_format="auto",
+        response_role="assistant",
+        request_logger=request_logger,
+    )
+
+    custom_router.generate = lambda s: OpenAIServingChat(
         engine,
         model_config,
         openai_serving_models,
@@ -131,10 +187,10 @@ def serve():
         openai_serving_models,
         request_logger=request_logger,
     )
-    
+
     web_app.state.enable_server_load_tracking = True
     web_app.state.server_load_metrics = 0
-    
+
     # # Run system info printing in a separate thread
     # import threading
     # import time
